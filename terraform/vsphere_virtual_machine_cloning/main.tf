74091196bf10d984a59998680321d6ca99084e52
Ricardo Conzatti <ricardoconzatti@hotmail.com>
2023-12-15 13:44:46 -0300

    Create main.tf

---
+++ terraform/vsphere_virtual_machine_cloning/main.tf
@@ -0,0 +1,77 @@
+##### PROVIDER #####
+terraform {
+  required_providers {
+    vsphere = {
+      source = "hashicorp/vsphere"
+      #version = "2.6.1"
+    }
+  }
+}
+
+provider "vsphere" {
+  vsphere_server       = var.vsphere_server
+  user                 = var.username
+  password             = var.password
+  allow_unverified_ssl = var.allow_unverified_ssl
+}
+
+##### DATA #####
+data "vsphere_datacenter" "datacenter" {
+  name = var.datacenter_name
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  name          = var.cluster_name
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = var.datastore_name
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_network" "network" {
+  name          = var.network_name
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = var.template_name
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+##### RESOURCE #####
+resource "vsphere_virtual_machine" "vm" {
+  name             = var.vm_name
+  annotation       = var.vm_annotation
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  num_cpus         = var.vm_cpu
+  memory           = var.vm_memory
+  guest_id         = data.vsphere_virtual_machine.template.guest_id
+  scsi_type        = data.vsphere_virtual_machine.template.scsi_type
+  disk {
+    label            = var.vm_disk_label
+    size             = data.vsphere_virtual_machine.template.disks.0.size
+    thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
+  }
+  network_interface {
+    network_id = data.vsphere_network.network.id
+	adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
+  }
+  
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+    customize {
+      linux_options {
+        host_name = var.vm_name
+        domain    = var.vm_custom_domain
+      }
+      network_interface {
+        ipv4_address = var.vm_custom_ip_address
+        ipv4_netmask = var.vm_custom_mask
+      }
+      ipv4_gateway = var.vm_custom_ip_gateway
+    }
+  }
+}
